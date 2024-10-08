c701eb7237bca9ffe7cae9f2310f9e634ffb4b8a
Abdul Rahman <abdulnsamba@gmail.com>
2024-10-09 01:24:49 +0300

    Added buttons and functions

--- SignUp/SignUp/MainPage.xaml.cs
+++ SignUp/SignUp/MainPage.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Popups;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -25,6 +26,26 @@ namespace SignUp
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        // On click, the method retrives the name from the name textbox and interpolates it in the message dialog popup to be displayed.
+        private async void btnregister_Click(object sender, RoutedEventArgs e)
+        {
+            var username = name.Text;
+            var messageinterface = new MessageDialog($"{username} has been succesfully registered");
+            await messageinterface.ShowAsync();
+        }
+
+        // On Click, the method sets the textBox Values to an empty string and displays a popup that notify you about the clearance
+        private async void btnreset_Click(object sender, RoutedEventArgs e)
+        {
+            name.Text = "";
+            email.Text = "";
+            password.Text = "";
+            confirmpass.Text = "";
+
+            var messageinterface = new MessageDialog("The fields have been cleared.!");
+            await messageinterface.ShowAsync();
         }
     }
 }
